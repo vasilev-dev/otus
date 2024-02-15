f3a95458c290f5ece8d2d7991c922af75839b73b
Dmitriy Vasilev <dmitriy.vasilev.dev@gmail.com>
2024-02-15 14:13:51 +0300

    renamed project

--- fastify/settings.gradle.kts
+++ fastify/settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "my-project"
+rootProject.name = "fastify"
 
 dependencyResolutionManagement {
     versionCatalogs {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,5 +10,5 @@ plugins {
 }
 rootProject.name = "otus"
 
-includeBuild("my-project")
+includeBuild("fastify")
 includeBuild("lessons")
